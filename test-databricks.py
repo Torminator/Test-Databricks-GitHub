7e91eb296b60611c7954d66df0480d344a1768d2
Torben Schick <torben.schick@adesso.de>
2021-01-18 12:56:43 +0100

    Update test-databricks.py

--- test-databricks.py
+++ test-databricks.py
@@ -1,12 +1,10 @@
 # COMMAND ----------
 %sql
 SHOW DATABASES LIKE "sbp*";
-# COMMAND ----------
 
 # COMMAND ----------
 %sql
 SHOW TABLES IN sbp_b900_public_work;
-# COMMAND ----------
 
 # COMMAND ----------
 %sql
@@ -17,20 +15,24 @@ CREATE TABLE sbp_b900_public_work.test_persons_databricks (
   favColor string
 );
 
+# COMMAND ----------
+%sql
 SHOW GRANT ON sbp_b900_public_work.test_persons_databricks;
-
 ALTER TABLE sbp_b900_public_work.test_persons_databricks owner to `AAD-GRP-bdap-sbx-dl-b900-public-contributor`;
 
+# COMMAND ----------
+%sql
 INSERT INTO sbp_b900_public_work.ttest_persons_databricks VALUES
   (1, "Alice Hammond", 38, "Blue"),
   (2, "Joy Meyer", 20, "Red"),
   (3, "Frieda Schmidt", 59, "Blue"),
   (4, "Dong Seok", 44, "Blue"),
   (5, "Camila Castillo", 32, "Green"),
-  (6, "Alba Cantu", 27, "Red");
-  
+  (6, "Alba Cantu", 27, "Red");  
 SELECT * FROM sbp_b900_public_work.test_persons_databricks;
 
+# COMMAND ----------
+%sql
 SELECT 
   favColor, 
   COUNT(*), 
@@ -39,6 +41,8 @@ FROM
   sbp_b900_public_work.test_persons_databricks
 GROUP BY
   favColor;
-
-DROP TABLE sbp_b900_public_work.test_persons_databricks;
+  
 # COMMAND ----------
+%sql
+DROP TABLE sbp_b900_public_work.test_persons_databricks;
+
